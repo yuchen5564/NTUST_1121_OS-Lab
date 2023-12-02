6333abc09285ff1afdcd97933b595b717e7a4240
Merlin Kuo <94113770+yuchen5564@users.noreply.github.com>
2023-12-02 17:47:23 +0800

    feat: change ssd_do_write

--- ssd_fuse.c
+++ ssd_fuse.c
@@ -147,20 +147,10 @@ static int nand_erase(int block)
     return 1;
 }
 
-// 2023/11/26 By yuchen
+// 2023/11/26 By jovi
 static unsigned int get_next_pca()
 {
-    /*  TODO: seq A, need to change to seq B */
-
-    // block: # of nand
-    // page: # of page
-
-    /*
-        seq-A: write every nand's page 0 then write every nand's page 1 ...
-        seq-B: write every page in nand 0 then write every page in nand 2 ...
-    */
-
-    if (curr_pca.pca == INVALID_PCA) // first time allocat
+    if (curr_pca.pca == INVALID_PCA)
     {
         // init
         curr_pca.pca = 0;
@@ -173,10 +163,12 @@ static unsigned int get_next_pca()
         printf("No new PCA\n");
         return FULL_PCA;
     }
-
-    printf(">>>>>>> page size: %d\n", (NAND_SIZE_KB * 1024 / 512));
-
-    if (curr_pca.fields.page >= (NAND_SIZE_KB * 1024 / 512) - 1 && (curr_pca.fields.block + 1) == PHYSICAL_NAND_NUM)
+    if (curr_pca.fields.page == NAND_SIZE_KB * (1024 / 512) - 1)
+    {
+        curr_pca.fields.block += 1;
+    }
+    curr_pca.fields.page = (curr_pca.fields.page + 1) % (NAND_SIZE_KB * (1024 / 512));
+    if (curr_pca.fields.block >= PHYSICAL_NAND_NUM)
     {
         printf("No new PCA\n");
         curr_pca.pca = FULL_PCA;
@@ -184,15 +176,6 @@ static unsigned int get_next_pca()
     }
     else
     {
-        if (curr_pca.fields.page >= (NAND_SIZE_KB * 1024 / 512) - 1)
-        {
-            curr_pca.fields.block = (curr_pca.fields.block + 1) % PHYSICAL_NAND_NUM;
-            curr_pca.fields.page = 0;
-        }
-        else
-        {
-            curr_pca.fields.page += 1;
-        }
         printf("PCA = page %d, nand %d\n", curr_pca.fields.page, curr_pca.fields.block);
         return curr_pca.pca;
     }
@@ -328,12 +311,9 @@ static int ssd_read(const char *path, char *buf, size_t size,
     return ssd_do_read(buf, size, offset);
 }
 
-// 2023/11/25 By yuchen.
 static int ssd_do_write(const char *buf, size_t size, off_t offset)
 {
-    printf(">>>>>> ssd_do_write\n");
     /*  TODO: only basic write case, need to consider other cases */
-
     int tmp_lba, tmp_lba_range, process_size;
     int idx, curr_size, remain_size, rst;
 
@@ -342,32 +322,44 @@ static int ssd_do_write(const char *buf, size_t size, off_t offset)
     {
         return -ENOMEM;
     }
-    printf(">>>>>> after ssd_expand\n");
 
     tmp_lba = offset / 512;
     tmp_lba_range = (offset + size - 1) / 512 - (tmp_lba) + 1;
-    printf(">>>> tmp_lba_range: %d\n", tmp_lba_range);
-
     process_size = 0;
     remain_size = size;
+    if (*(buf + remain_size - 1) == '\n')
+        remain_size--;
     curr_size = 0;
 
-    printf(">>>> Input Size: %ld\n", size);
+    // consider case:
+    //   [x] 1. offset align 512 && input size align 512 (sample code)
+    //   [x] 2. offset align 512 && input size "not" align 512 (yuchen)
+    //   [x] 3. offset "not" align 512 && input size align 512
+    //   [x] 4. offset "not" align 512 && input size "not" align 512
+
+    // process flow:
+    //   if size || offset not align 512, should read the data from nand and overwrite it
+    char alignBuf[512] = {'\0'};
 
     for (idx = 0; idx < tmp_lba_range; idx++)
     {
         /*  example only align 512, need to implement other cases  */
+
         if (offset % 512 == 0)
         {
-
-            char alignBuf[512] = {'\0'};
-            if (remain_size > 512)
-                memcpy(&alignBuf, buf + process_size, 512);
+            printf(">>>>> ssd_do_write Case 1\n");
+            if (!ftl_read(alignBuf, tmp_lba)) // LBA space is clear
+            {
+                rst = ftl_write(buf + process_size, 1, tmp_lba + idx);
+            }
             else
-                memcpy(&alignBuf, buf + process_size, remain_size);
-
-            // rst = ftl_write(buf + process_size, 1, tmp_lba + idx);
-            rst = ftl_write(alignBuf, 1, tmp_lba + idx);
+            {
+                if (remain_size > 512)
+                    memcpy(&alignBuf, buf + process_size, 512);
+                else
+                    memcpy(&alignBuf, buf + process_size, remain_size);
+                rst = ftl_write(alignBuf, 1, tmp_lba + idx);
+            }
             if (rst == 0)
             {
                 // write full return -enomem;
@@ -383,9 +375,42 @@ static int ssd_do_write(const char *buf, size_t size, off_t offset)
             process_size += 512;
             offset += 512;
         }
+        else if (offset % 512 != 0)
+        {
+            ftl_read(alignBuf, tmp_lba);
+            int offset_in_lba = offset % 512;
+            if ((offset + remain_size) / 512 == tmp_lba) // all in same lba page
+            {
+                printf(">>>>> ssd_do_write Case 2\n");
+                memcpy(alignBuf + offset_in_lba, buf, remain_size);
+                rst = ftl_write(alignBuf, 1, tmp_lba + idx);
+            }
+            else
+            {
+                printf(">>>>> ssd_do_write Case 3\n");
+                memcpy(alignBuf + offset_in_lba, buf, 512 - offset_in_lba);
+                rst = ftl_write(alignBuf, 1, tmp_lba + idx);
+            }
+            
+
+            if (rst == 0)
+            {
+                // write full return -enomem;
+                return -ENOMEM;
+            }
+            else if (rst < 0)
+            {
+                // error
+                return rst;
+            }
+            curr_size += 512;
+            remain_size -= (512 - offset_in_lba);
+            process_size += (512 - offset_in_lba);
+            offset += (512 - offset_in_lba);
+        }
         else
         {
-            printf(" --> Not align 512 !!!\n");
+            printf(" --> Something Wrong at ssd_do_write function...\n");
             return -EINVAL;
         }
     }
